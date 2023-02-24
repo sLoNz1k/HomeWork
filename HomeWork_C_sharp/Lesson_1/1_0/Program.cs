c2f25b91cbe66dc6bb1847d6771f948005b0d22c
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-02-25 00:22:33 +0300

    task_2 and edited task_1

--- HomeWork_C_sharp/Lesson_1/1_0/Program.cs
+++ HomeWork_C_sharp/Lesson_1/1_0/Program.cs
@@ -1,4 +1,6 @@
-﻿int firstNum = int.Parse(Console.ReadLine()!);
+﻿Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
 int secondNum = int.Parse(Console.ReadLine()!);
 if (firstNum > secondNum)
 {
