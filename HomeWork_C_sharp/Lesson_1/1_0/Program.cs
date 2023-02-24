249f451e5c4413022becf6f5351395adc21268a0
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-02-24 22:47:39 +0300

    task_1

---
+++ HomeWork_C_sharp/Lesson_1/1_0/Program.cs
@@ -0,0 +1,10 @@
+﻿int firstNum = int.Parse(Console.ReadLine()!);
+int secondNum = int.Parse(Console.ReadLine()!);
+if (firstNum > secondNum)
+{
+    Console.Write($"{firstNum} больше {secondNum}");
+}
+else
+{
+    Console.Write($"{secondNum} больше {firstNum}");
+}    
