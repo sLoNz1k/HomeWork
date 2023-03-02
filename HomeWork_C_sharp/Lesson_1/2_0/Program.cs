a60ac4ac71fafc978a17fcac3a94cea753b1f456
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-02 22:28:51 +0300

    completed 3 and edited 1

--- HomeWork_C_sharp/Lesson_1/2_0/Program.cs
+++ HomeWork_C_sharp/Lesson_1/2_0/Program.cs
@@ -1,8 +1,27 @@
-﻿int num = int.Parse(Console.ReadLine()!);
-int TakeNum(int num)
+﻿int Prompt(string message)
 {
-    Console.WriteLine(num);
-    return (num /10 %10);
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
 }
-int result = TakeNum(num);
-Console.WriteLine(result);
+bool checkout(int num)
+{
+    if(num >= 100 && num <= 999)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int num = Prompt("Введите число: ");
+if(checkout(num))
+{
+    Console.WriteLine(num /10 % 10);
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число!");
+}
