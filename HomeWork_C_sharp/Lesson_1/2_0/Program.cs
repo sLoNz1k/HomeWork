cade94a534fd5dfa12c57928d862dbf58f599174
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-02 18:40:17 +0300

    first HW

--- HomeWork_C_sharp/Lesson_1/2_0/Program.cs
+++ HomeWork_C_sharp/Lesson_1/2_0/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = int.Parse(Console.ReadLine()!);
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return (num /10 %10);
+}
+int result = TakeNum(num);
+Console.WriteLine(result);
