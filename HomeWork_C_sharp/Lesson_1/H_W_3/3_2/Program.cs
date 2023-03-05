eabdbf8150a9651d73796346362a6890658eb1a0
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-05 22:46:09 +0300

    complete dz2

---
+++ HomeWork_C_sharp/Lesson_1/H_W_3/3_2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+bool test (int num)
+{
+    if(num >= 10000 && num <= 99999)
+        return true;
+    else
+    {
+        Console.WriteLine("Number isnt five-digit");
+        return false;
+    }
+}
+void pallindrome(int num)
+{   
+    bool test_num = test(num);
+    if (test_num == true)
+    {
+        if(num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)
+            Console.WriteLine($"{num} -> is Pallindrome");
+        else
+        {
+            Console.WriteLine($"{num} -> isnt Pallindrome");
+        }
+    }
+}
+pallindrome(int.Parse(Console.ReadLine())!);
