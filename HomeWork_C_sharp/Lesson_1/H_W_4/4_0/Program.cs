afc338cf594861afabb753955043a14d7272c3d7
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-09 20:30:36 +0300

    complete 1

--- HomeWork_C_sharp/Lesson_1/H_W_4/4_0/Program.cs
+++ HomeWork_C_sharp/Lesson_1/H_W_4/4_0/Program.cs
@@ -2,6 +2,9 @@
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите степень:");
 int b = int.Parse(Console.ReadLine()!);
-double natural = Math.Pow(a, b);
-Console.WriteLine("Введите число: ");
-Console.WriteLine(natural);
+int result = a;
+for (int i = 1; i < b; i++)
+{
+    result = a * result;
+}
+Console.WriteLine(result);
