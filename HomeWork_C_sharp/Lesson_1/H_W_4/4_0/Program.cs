a2a46db726592982ce47c425f59b181e917c9a5e
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-09 20:17:11 +0300

    complete 3

---
+++ HomeWork_C_sharp/Lesson_1/H_W_4/4_0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число возводимое в степень: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень:");
+int b = int.Parse(Console.ReadLine()!);
+double natural = Math.Pow(a, b);
+Console.WriteLine("Введите число: ");
+Console.WriteLine(natural);
---
+++ HomeWork_C_sharp/Lesson_1/H_W_4/4_2/Program.cs
@@ -0,0 +1,10 @@
+﻿void FillMassive(int[] mass)
+{
+    for(int i = 0;i<mass.Length;i++)
+    {
+        mass[i] = new Random().Next(1,15);
+        Console.Write($"{mass[i]} ");
+    }
+}
+int[] array = new int[8];
+FillMassive(array);
