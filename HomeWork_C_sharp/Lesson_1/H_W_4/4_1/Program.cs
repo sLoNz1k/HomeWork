df5ef1fa64ff89d84d76899f1179d268cf837ea3
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-10 16:27:54 +0300

    completed

--- HomeWork_C_sharp/Lesson_1/H_W_4/4_1/Program.cs
+++ HomeWork_C_sharp/Lesson_1/H_W_4/4_1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int result = 0;
+while(num > 0)
+{
+    int temp = num % 10;
+    result = temp + result;
+    num = num / 10;
+
+}
+Console.Write(result);
