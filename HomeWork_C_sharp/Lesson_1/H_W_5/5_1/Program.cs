4baa133301a25d2db1a9b91ac65a99e59fd8e8fb
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-12 22:50:30 +0300

    complete 5_1

--- HomeWork_C_sharp/Lesson_1/H_W_5/5_1/Program.cs
+++ HomeWork_C_sharp/Lesson_1/H_W_5/5_1/Program.cs
@@ -1,1 +1,43 @@
-﻿int[] 
+﻿int[] Array(int massive)
+{
+    int[] mass = new int[massive];
+    for (int i = 0; i < massive; i++)
+    {
+        mass[i] = new Random().Next(1, 10);
+
+
+    }
+    return mass;
+}
+void PrintMassive(int[] arry)
+{
+    int Lenght = arry.Length;
+    for (int i = 0; i < Lenght; i++)
+    {
+        Console.Write($"{arry[i]} ");
+    }
+}
+
+int SumOddNums(int[] array)
+{
+    int SumNum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1)
+            SumNum += array[i];
+    }
+    if (SumNum != 0)
+    {
+        Console.Write($"Сумма нечетных чисел = {SumNum}");
+        return SumNum;
+    }
+    else
+    {
+        Console.Write("Нечётных чисел нет!");
+        return SumNum;
+    }
+}
+int[] Mass = Array(3);
+PrintMassive(Mass);
+Console.WriteLine();
+SumOddNums(Mass);
