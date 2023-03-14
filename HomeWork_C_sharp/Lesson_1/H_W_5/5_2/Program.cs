b4bf38d8b009ae7dc091f9bf9bb81a2fafa1532a
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-14 18:51:44 +0300

    5_1,5_2

---
+++ HomeWork_C_sharp/Lesson_1/H_W_5/5_2/Program.cs
@@ -0,0 +1,36 @@
+﻿double[] Array(int MassiveSize)
+{
+    double[] mass = new double[MassiveSize];
+    for (int i = 0; i < MassiveSize; i++)
+    {
+        mass[i] = new Random().NextDouble();
+    }
+    return mass;
+}
+
+
+void PrintMassive(double[] array)
+{
+    int Lenght = array.Length;
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
+
+void Substraction(double[] massive)
+{
+    double max = massive[0];
+    double min = massive[0];
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i] > max)
+            max = massive[i];
+        else if (massive[i] < min)
+            min = massive[i];
+
+    }
+    Console.WriteLine($"Разница между {max} и {min} равна = {max - min}");
+}
+double[] size = Array(20);
+PrintMassive(size);
+Console.WriteLine();
+Substraction(size);
