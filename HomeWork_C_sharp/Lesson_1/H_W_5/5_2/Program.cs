47d3337d4a928317e21689358a8755417ddd425f
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-17 19:31:23 +0300

    complete 6

--- HomeWork_C_sharp/Lesson_1/H_W_5/5_2/Program.cs
+++ HomeWork_C_sharp/Lesson_1/H_W_5/5_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿double[] Array(int MassiveSize)
 {
     double[] mass = new double[MassiveSize];
+    Random n_new = new Random();
     for (int i = 0; i < MassiveSize; i++)
     {
         mass[i] = new Random().NextDouble();
@@ -28,9 +29,51 @@ void Substraction(double[] massive)
             min = massive[i];
 
     }
-    Console.WriteLine($"Разница между {max} и {min} равна = {max - min}");
+    Console.WriteLine(($"Разница между {max} и {min} равна = {max - min}"));
 }
 double[] size = Array(20);
 PrintMassive(size);
 Console.WriteLine();
 Substraction(size);
+// double[] Array(int size, int from, int to)
+// {
+//     double[] arr = new double[size];
+//     Random n_new = new Random();
+//     for (int i = 1; i < size; i++)
+//     {
+//         arr[i] = Math.Round(n_new.NextDouble() * (from + to) - from, 2);
+//     }
+//     return arr;
+// }
+
+// void PrintArray(double[] array)
+// {
+//     int size = array.Length;
+//     for (int i = 0; i < size; i++)
+//         Console.Write($"{array[i]} ");
+// }
+
+// void DiffMaxMin(double[] arr)
+// {
+//     double n_max = arr[0];
+//     double n_min = arr[0];
+
+//     for (int i = 1; i < arr.Length; i++)
+//     {
+//         if (n_max < arr[i])
+//             n_max = arr[i];
+//         else if (n_min > arr[i])
+//             n_min = arr[i];
+//     }
+//     Console.Write($"Max: {n_max} , Min: {n_min}. ");
+//     Console.Write($"Difference: {n_max} - ({n_min}) = {Math.Round(n_max - n_min , 2)} ");
+
+// }
+// double[] arr_1 = Array(int.Parse(Console.ReadLine()),
+//                     int.Parse(Console.ReadLine()),
+//                     int.Parse(Console.ReadLine()));
+// PrintArray(arr_1);
+// DiffMaxMin(arr_1);
+
+
+
---
+++ HomeWork_C_sharp/Lesson_1/H_W_6/6_0/Program.cs
@@ -0,0 +1,25 @@
+﻿// string s = "15 20 30 40 50";
+
+// string[] subs = s.Split('0');
+
+// foreach (var sub in subs)
+// {
+//     Console.WriteLine($"Substring: {sub}");
+// }
+
+int Check(int number)
+{
+    int count = 0;
+    
+    for(int i = 0; i <= number; i ++)
+    {
+        Console.Write($"Число {i}: ");
+        int a = int.Parse(Console.ReadLine()!);
+        if(a > 0) count ++;
+    }
+    return count;
+}
+Console.Write("Введите количество чисел: ");
+int Convert = int.Parse(Console.ReadLine()!);
+int result = Check(Convert);
+Console.WriteLine($"Количество чисел больше нуля: {result}");
