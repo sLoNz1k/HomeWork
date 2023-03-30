351c6cb3f86ba3ee0d55b3c9e0e2e389ba68f28c
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-03-30 11:59:51 +0300

    complete 9_1

---
+++ HomeWork_C_sharp/Lesson_1/H_W_9/9_1/Program.cs
@@ -0,0 +1,15 @@
+﻿void EvenNums(int M, int N)
+{
+    if(N < M) return;
+        Console.Write($"{M} ");
+    EvenNums(M+2, N);
+
+}
+Console.WriteLine("Введите стартовое число: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конечное число: ");
+int N = int.Parse(Console.ReadLine()!);
+if (M % 2!=0)
+    M = M +1;
+    
+EvenNums(M,N);
